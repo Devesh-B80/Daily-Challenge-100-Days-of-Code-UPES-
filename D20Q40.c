68482cdad5cfb80563b0462b14e7753b9a7374e5
Devesh-B80 <Devesh.29077@stu.upes.ac.in>
2025-11-09 16:49:30 +0530

    Add files via upload

---
+++ D20Q40.c
@@ -0,0 +1,43 @@
+//Q40: Write a program to find the 1’s complement of a binary number and print it.
+
+/*
+Sample Test Cases:
+Input 1:
+1010
+Output 1:
+0101
+
+Input 2:
+1111
+Output 2:
+0000
+
+*/
+//solution:-
+#include <stdio.h>
+
+int main() {
+    char binary[50];
+    int i;
+
+    // Input
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    // Find 1's complement
+    for (i = 0; binary[i] != '\0'; i++) {
+        if (binary[i] == '0')
+            binary[i] = '1';
+        else if (binary[i] == '1')
+            binary[i] = '0';
+        else {
+            printf("Invalid binary number\n");
+            return 0;
+        }
+    }
+
+    // Output
+    printf("%s\n", binary);
+
+    return 0;
+}
