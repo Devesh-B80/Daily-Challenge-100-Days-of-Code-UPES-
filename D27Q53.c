68103eca55e9421146a94386f18f488779c84c05
Devesh-B80 <Devesh.29077@stu.upes.ac.in>
2025-11-09 17:40:01 +0530

    Add files via upload

---
+++ D27Q53.c
@@ -0,0 +1,51 @@
+/*Q53: Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+//solution:-
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Upper half of the pattern
+    for(i = 1; i <= 5; i++) {
+        for(j = 1; j <= 2*i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower half of the pattern
+    for(i = 4; i >= 1; i--) {
+        for(j = 1; j <= 2*i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
