cda1b577725bed3dd5d76aa1f92a5770f0d64109
Devesh-B80 <Devesh.29077@stu.upes.ac.in>
2025-11-12 14:55:40 +0530

    Add files via upload

---
+++ D36Q72.c
@@ -0,0 +1,35 @@
+/*Q72: Find the sum of all elements in a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+21
+
+*/
+//solution:-
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j, sum = 0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j]; // Add to sum directly
+        }
+    }
+
+    printf("Sum of all elements: %d\n", sum);
+
+    return 0;
+}
